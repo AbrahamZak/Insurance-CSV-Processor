4c9f909144f0bf32d109ef09a6326682cadb7d67
DESKTOP-95N3UOJ\abrah <abrahamzakharov@gmail.com>
2021-09-02 23:14:26 -0400

    Replaced open with a csv dict reader to load in csvs as dictionaries, retaining key/value pairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 if __name__ == '__main__':
     # This is the directory containing all csv files to be processed, feel free to change it!
@@ -7,15 +8,7 @@ if __name__ == '__main__':
     for filename in os.listdir(input_dir):
         print(filename)
         # Open the file at: input directory / filename
+        reader = csv.DictReader(open(input_dir + '/' + filename, 'r'))
         with open(input_dir + '/' + filename, 'r') as f:
-            # Skip the header
-            next(f)
-            # Read all lines
-            lines = f.readlines()
-        # Now we can process each line from the file
-        for line in lines:
-            # Strip the right newline character and then split the line by comma
-            line = line.rstrip()
-            data = line.split(',')
-            print(data)
-
+            for row in reader:
+                print(row)
